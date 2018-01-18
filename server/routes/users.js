a97d7b567b5b74cbb498a7ff617258f4d96b5d08
mxdi9i7 <peter.zheng88228@gmail.com>
2018-01-18 11:13:59 -0500

    merge

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var { handleUserLogin } = require('../controllers/users/auth')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  handleUserLogin(req, res, next)
 });
 
 module.exports = router;
