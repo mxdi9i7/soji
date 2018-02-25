8908f6033bb02d7a8dcc67beed35ded53793bcca
mxdi9i7 <peter.zheng88228@gmail.com>
2018-02-25 10:02:06 -0500

    simple fast push

--- client/src/components/dashboard/DashboardPage.js
+++ client/src/components/dashboard/DashboardPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Dashnav from '../partials/Dashnav';
 import SideNav from '../partials/SideNav';
+import '../../assets/dash.css'
 export default class DashboardPage extends Component {
 
     render() {
@@ -8,6 +9,19 @@ export default class DashboardPage extends Component {
             <div>
                 <Dashnav />
                 <SideNav />
+                <div className="dashContent">
+                    <div className="dashHeader">
+                        <div className="dashTitle">
+                            <h1>Dashboard</h1>
+                        </div>
+                        <div className="dashActions">
+                            <button>
+                                <i className="fa fa-plus"></i>Create a New Job
+                            </button>
+                            <i className="fa fa-search"></i><input type="text" placeholder="Search"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
